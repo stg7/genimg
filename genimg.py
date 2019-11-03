725ea16eea9d67213dc7eac8a3902bd4590edaf5
Steve <stg7@gmx.de>
2019-11-04 00:26:43 +0100

    change default number of shapes, change output to SVG

--- genimg.py
+++ genimg.py
@@ -7,8 +7,45 @@ import multiprocessing
 
 import skimage.io
 import skimage.draw
+import skimage.transform
 import sklearn.metrics
 import numpy as np
+
+
+
+
+class SVG:
+    def __init__(self, backgroundcolor, shape):
+        assert(len(shape) == 2)
+        assert(len(backgroundcolor) == 3)
+        self._backgroundcolor = backgroundcolor
+        self._shape = shape
+        self._triangles = []
+
+    def html_color(self, color):
+        return "rgb(" + ",".join([str(int(256 * x)) for x in color]) + f")"
+
+    def draw_triangle(self, triangle, color):
+        assert(len(triangle) == 3)
+        assert(len(color) == 3)
+        self._triangles.append((triangle, color))
+
+    def store_svg(self, svg_filename):
+        triangles = ""
+        for triangle in self._triangles:
+            t, c = triangle
+            coords = " ".join([f"{int(self._shape[1] * i[1])},{int(self._shape[0] * i[0])}" for i in t])
+            triangles += f"""\
+    <polygon points="{coords}" fill="{self.html_color(c)}" fill-opacity="0.5" />
+            """
+        svg = f"""<?xml version="1.0" encoding="UTF-8"?>
+<svg height="{self._shape[0]}" width="{self._shape[1]}">
+  <polygon points="0,0 0,{self._shape[0]} {self._shape[1]},{self._shape[0]} {self._shape[1]},0" fill="{self.html_color(self._backgroundcolor)}" />
+  {triangles}
+</svg>
+        """
+        with open(svg_filename, "w") as svg_fp:
+            svg_fp.write(svg)
 
 
 def rmse(x, y):
@@ -71,9 +108,9 @@ def main(_):
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("image", type=str, help="image to process")
     parser.add_argument('--cpu_count', type=int, default=multiprocessing.cpu_count(), help='thread/cpu count')
-    parser.add_argument('--num_shapes', type=int, default=200, help='number of used shapes')
+    parser.add_argument('--num_shapes', type=int, default=20000, help='number of used shapes')
     parser.add_argument(
-        "--output_file", type=str, default="image.png", help="generated resulting image"
+        "--output_file", type=str, default="image.svg", help="generated resulting image"
     )
     a = vars(parser.parse_args())
     random.seed(42)
@@ -92,13 +129,14 @@ def main(_):
 
     current[:,:] = mean_color
 
-    high_res = np.zeros((2000, int(2000 * aspect_ratio), 3))
-    high_res[:,:] = mean_color
+    svg_image = SVG(mean_color, (2000, int(2000 * aspect_ratio)))
+
     last_rmse = rmse(current, img)
     mutation_rate = 0.5
     filename = os.path.basename(a["output_file"])
     output_dir = os.path.dirname(a["output_file"])
-    # todo: add SVG output
+    os.makedirs(output_dir, exist_ok=True)
+
     for j in range(num_shapes):
         candidates = sum(pool.starmap(check_triangle, [(current, img, mutation_rate, last_rmse) for l in range(100)]), [])
         candidates.sort(key=lambda x: (x[0] - last_rmse) * (img.shape[0] * img.shape[1] - x[2] * j/num_shapes))
@@ -106,9 +144,8 @@ def main(_):
         last_rmse, triangle, area = candidates[0]
         print(f"step: {j}, rmse: {candidates[0][0]}, len(c)={len(candidates)}, area={area}")
         current, color, _ = draw_triangle(current, triangle, img)
-        high_res, _, _ = draw_triangle(high_res, triangle, color=color)
-
-        skimage.io.imsave(output_dir + "/" + str(j) + "_" + filename, skimage.img_as_ubyte(high_res))
+        svg_image.draw_triangle(triangle, color)
+        svg_image.store_svg(output_dir + "/" + str(j) + "_" + filename)
 
 
 if __name__ == "__main__":
